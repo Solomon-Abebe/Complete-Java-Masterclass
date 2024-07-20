5eaee591ced2277c696f57509a130a38a8bcc568
Solomon-Abebe <solomonabebe23@gmail.com>
2024-07-20 11:47:33 +0300

    Ternary Operator added

--- src/Section3FirstStep/Hello.java
+++ src/Section3FirstStep/Hello.java
@@ -33,5 +33,15 @@ public class Hello {
         if (!isCar) {
             System.out.println("This is not supposed to happen");
         }
+        String makeOfCar = "Volkswagen";
+        boolean isDomestic = makeOfCar == "Volkswagen" ? false : true;
+
+        if (isDomestic) {
+            System.out.println("This car is domestic to our country");
+        }
+
+        String s = (isDomestic) ? "This car is domestic" : "This car is not domestic";
+
+        System.out.println(s);
     }
 }
