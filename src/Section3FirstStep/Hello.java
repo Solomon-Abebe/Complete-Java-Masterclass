461f3e5e4c7061b804495c1b40c3238736921f85
Solomon-Abebe <solomonabebe23@gmail.com>
2024-07-20 11:52:15 +0300

    Operator precedence and operator challenge

--- src/Section3FirstStep/Hello.java
+++ src/Section3FirstStep/Hello.java
@@ -43,5 +43,17 @@ public class Hello {
         String s = (isDomestic) ? "This car is domestic" : "This car is not domestic";
 
         System.out.println(s);
+//Operator precedence and operator challenge
+        double myFirstValue = 20.00d;
+        double mySecondValue = 80.00d;
+        double myValuesTotal = (myFirstValue + mySecondValue) * 100.00d;
+        System.out.println("MyValuesTotal = " + myValuesTotal);
+        double theRemainder = myValuesTotal % 40.00d;
+        System.out.println("theRemainder = " + theRemainder);
+        boolean isNoRemainder = (theRemainder == 0) ? true : false;
+        System.out.println("isNoRemainder = " + isNoRemainder);
+        if (!isNoRemainder) {
+            System.out.println("Got some remainder");
+        }
     }
 }
