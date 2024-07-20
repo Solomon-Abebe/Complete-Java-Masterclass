a7b9fca045d18faa8dbaf6833342e4d324a2b558
Solomon-Abebe <solomonabebe23@gmail.com>
2024-07-20 15:30:39 +0300

    Methods in java more effective way

--- src/Section5ExpressionsStatements/MethodsInJava.java
+++ src/Section5ExpressionsStatements/MethodsInJava.java
@@ -3,16 +3,8 @@ package Section5ExpressionsStatements;
 public class MethodsInJava {
 
     public static void main(String[] args) {
-        boolean gameOver = true;
-        int score = 500;
-        int levelCompleted =4;
-        int bonus =600;
 
-        int finalScore = score;
-        if(gameOver){
-            finalScore += (levelCompleted * bonus);
-            System.out.println("Your final score wassss: " + finalScore);
-        }
+        calculateScores(true, 500, 4, 600);
         calculateScores(true, 8000, 6, 150);
     }
     public static void calculateScores(boolean gameOver, int score, int levelCompleted, int bonus){
