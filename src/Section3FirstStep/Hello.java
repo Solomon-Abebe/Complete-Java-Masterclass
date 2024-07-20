f1c6e1292422d0cf18ceaa300cdb4d0ffa137d50
Solomon-Abebe <solomonabebe23@gmail.com>
2024-07-20 11:36:58 +0300

    Or operator added

--- src/Section3FirstStep/Hello.java
+++ src/Section3FirstStep/Hello.java
@@ -15,10 +15,13 @@ public class Hello {
             System.out.println("You got the high score!");
         }
 
-        int secondTopScore = 81;
-        if (topScore > secondTopScore && topScore < 100) {
+        int secondTopScore = 95;
+        if ((topScore > secondTopScore) && (topScore < 100)) {
             System.out.println("Greater than second top score and less than 100");
         }
 
+        if ((topScore > 90) || (secondTopScore <= 90)) {
+            System.out.println("Either or both of the conditions are true");
+        }
     }
 }
