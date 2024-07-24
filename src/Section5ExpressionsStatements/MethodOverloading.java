cf16fba6a169652a28ef60ff7a73044750e37bb1
Solomon-Abebe <solomonabebe23@gmail.com>
2024-07-24 11:28:36 +0300

    Method Overloading

---
+++ src/Section5ExpressionsStatements/MethodOverloading.java
@@ -0,0 +1,26 @@
+package Section5ExpressionsStatements;
+
+public class MethodOverloading {
+    public static void main(String[] args) {
+
+        System.out.println("New score is " + calculateScore("Tim", 500));
+        System.out.println("New score is " + calculateScore(10));
+    }
+
+    public static int calculateScore(String playerName, int score) {
+
+        System.out.println("Player " + playerName + " scored " + score + " points");
+        return score * 1000;
+    }
+
+    public static int calculateScore(int score) {
+
+        return calculateScore("Anonymous", score);
+    }
+
+    public static int calculateScore() {
+
+        System.out.println("No player name, no player score.");
+        return 0;
+    }
+}
