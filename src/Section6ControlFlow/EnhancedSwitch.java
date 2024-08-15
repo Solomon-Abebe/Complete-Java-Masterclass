96d3d752120fe9f4bde2304b13b16f870465c1d5
Solomon-Abebe <solomonabebe23@gmail.com>
2024-08-15 23:18:56 +0300

    Switch statement, traditional and enhanced

--- src/Section6ControlFlow/EnhancedSwitch.java
+++ src/Section6ControlFlow/EnhancedSwitch.java
@@ -2,26 +2,37 @@ package Section6ControlFlow;
 
 public class EnhancedSwitch {
     public static String getQuarterEnhanced(String month) {
-        switch (month) {
-            case "JANUARY", "FEBRUARY", "MARCH" -> {
-                return "1st Quarter";
-            }
-            case "APRIL", "MAY", "JUNE" -> {
-                return "2nd Quarter";
-            }
-            case "JULY", "AUGUST", "SEPTEMBER" -> {
-                return "3rd Quarter";
-            }
-            case "OCTOBER", "NOVEMBER", "DECEMBER" -> {
-                return "4th Quarter";
-            }
-        }
-        return "bad";
+       return switch (month) {
+
+            case "JANUARY", "FEBRUARY", "MARCH" -> "1st Quarter";
+            case "APRIL", "MAY", "JUNE" -> "2nd Quarter";
+            case "JULY", "AUGUST", "SEPTEMBER" -> "3rd Quarter";
+            case "OCTOBER", "NOVEMBER", "DECEMBER" -> "4th Quarter";
+            default -> "bad";
+
+
+           //            case "JANUARY", "FEBRUARY", "MARCH" -> {
+//                return "1st Quarter";
+//            }
+//            case "APRIL", "MAY", "JUNE" -> {
+//                return "2nd Quarter";
+//            }
+//            case "JULY", "AUGUST", "SEPTEMBER" -> {
+//                return "3rd Quarter";
+//            }
+//            case "OCTOBER", "NOVEMBER", "DECEMBER" -> {
+//                return "4th Quarter";
+//            }
+//        }
+//        return "bad";
+
+        };
     }
+
 
 
     public static void main(String[] args) {
 
-        System.out.println(getQuarterEnhanced("DECEMBER"));
+        System.out.println(getQuarterEnhanced("FEBRUARY"));
     }
 }
